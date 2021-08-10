4464389c8cd7392b4bf4bc5f13db2bb03d31cd2b
thebatzuk <rulisr2c@gmail.com>
2021-08-10 12:51:32 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
-console.log("Hello World!")
-var w3r_text = "This text will be added to HTML";
-var newParagraph = document.createElement("p"); //creates a new paragraph element
-var newText = document.createTextNode(w3r_text); //creates text along with output to be displayed 
-newParagraph.appendChild(newText); //created text is appended to the paragraph element created
-document.body.appendChild(newParagraph); // created paragraph and text along with output is appended to the document body
+//console.log("Hello World!")
+var
+canvas    = document.querySelector('canvas'),
+ctx       = canvas.getContext('2d'),
+beginPath = false;
+
+canvas.width  = window.innerWidth;
+canvas.height = window.innerHeight;
+
+document.body.addEventListener('mousemove', function (event) {
+	var x = event.clientX, y = event.clientY;
+
+	if (beginPath) {
+		ctx.lineTo(x, y);
+		ctx.stroke();
+	} else {
+		ctx.beginPath();
+		ctx.moveTo(x, y);
+		beginPath = true;
+	}
+}, false);
