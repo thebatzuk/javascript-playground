a3b3e8ce52e9108afc5ad17e1fb884fe434ae928
thebatzuk <rulisr2c@gmail.com>
2021-08-10 13:09:11 -0600

    local update

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
 console.log("Hello World!")
+
+let text = "This was added via JS";
+var newParagraph = document.createElement("p"); //creates a new paragraph element
+var newText = document.createTextNode(text); //creates text along with output to be displayed 
+newParagraph.appendChild(newText); //created text is appended to the paragraph element created
+document.body.appendChild(newParagraph); // created paragraph and text along with output is appended to the document body
