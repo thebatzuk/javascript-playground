adf1a3af39f5fe13abf43ed8228e0e66e7fbba3c
thebatzuk <rulisr2c@gmail.com>
2021-08-11 12:27:02 -0600

    update script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
 console.log("Hello World!")
 
-let text = "This was added via JS";
-var newParagraph = document.createElement("p"); //creates a new paragraph element
-var newText = document.createTextNode(text); //creates text along with output to be displayed 
-newParagraph.appendChild(newText); //created text is appended to the paragraph element created
-document.body.appendChild(newParagraph); // created paragraph and text along with output is appended to the document body
+document.body.onclick = addElement
+
+function addElement() {
+
+    let newDiv = document.createElement("div")
+    let newContent = document.createTextNode("Text added via 'onclick'")
+    newDiv.appendChild(newContent)
+
+    let currentDiv = document.getElementById("start")
+    document.insertBefore(newDiv, currentDiv)
+
+}
