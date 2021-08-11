e6d2c9e44429ee442e77c23324ff13a999018b23
thebatzuk <rulisr2c@gmail.com>
2021-08-11 12:53:48 -0600

    fixed onclick behaviour

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,20 @@
 console.log("Hello World!")
 
-document.body.onclick = addElement
+document.body.onload = init
+
+function init() {
+    var titleElem = document.getElementById("title")
+    titleElem.onclick = addElement
+}
 
 function addElement() {
 
-    let newDiv = document.createElement("div")
-    let newContent = document.createTextNode("Text added via 'onclick'")
+    var newDiv = document.createElement("div")
+    var newContent = document.createTextNode("Text added via 'onclick'")
     newDiv.appendChild(newContent)
-
-    let currentDiv = document.getElementById("start")
-    document.insertBefore(newDiv, currentDiv)
+    
+    var mainSection = document.getElementById("main")
+    var currentDiv = document.getElementById("start")
+    mainSection.insertBefore(newDiv, currentDiv)
 
 }
